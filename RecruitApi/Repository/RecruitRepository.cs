47c522d589c452b9c8819e874d53e1b52d51e688
Ivan Dcosta <teneleven5664@gmail.com>
2023-12-01 21:09:20 +0530

    Test comment removed 12/1/2023 21:09

--- RecruitApi/Repository/RecruitRepository.cs
+++ RecruitApi/Repository/RecruitRepository.cs
@@ -29,7 +29,6 @@ namespace Recruit.Repository
             if (pageSize > 0)
             {
                 query = query.Skip(pageSize * (pageNumber - 1)).Take(pageSize);
-                //test
             }
 
             if (includeProperties != null)
